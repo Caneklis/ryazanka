6b744b1fd58ad24abdf4b8b6e90b2da0bec81673
alesukhin <alesukhin@museumplus.ru>
2021-09-05 13:33:43 +0300

    add new sections

--- src/js/main.js
+++ src/js/main.js
@@ -20,8 +20,11 @@ document.addEventListener("DOMContentLoaded", () => {
     },
     breakpoints: {
       768: {
-        slidesPerView: 2,
+        slidesPerView: 1,
         spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 2,
       },
     },
   });
@@ -49,4 +52,36 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     },
   });
+
+  var image = document.getElementsByClassName("thumbnail");
+  new simpleParallax(image, {
+    overflow: true,
+    delay: 0.6,
+    transition: "cubic-bezier(0,0,0,1)",
+    maxTransition: 60,
+  });
+
+  // // Parallax effect
+  // // Adapted from @ilonacodes article ->  https://link.medium.com/7fFiON6Q1X
+
+  // // Update : added throttle to increase performance
+  // window.addEventListener("scroll", throttle(parallax, 14));
+
+  // function throttle(fn, wait) {
+  //   var time = Date.now();
+  //   return function () {
+  //     if (time + wait - Date.now() < 0) {
+  //       fn();
+  //       time = Date.now();
+  //     }
+  //   };
+  // }
+
+  // function parallax() {
+  //   var scrolled = window.pageYOffset;
+  //   var parallax = document.querySelector(".parallax");
+  //   // You can adjust the 0.4 to change the speed
+  //   var coords = scrolled * 0.4 + "px";
+  //   parallax.style.transform = "translateY(" + coords + ")";
+  // }
 });
