1892496528fbac84445c4fe01505d1ba5b36ce00
alesukhin <alesukhin@museumplus.ru>
2021-09-20 17:08:53 +0300

    fix some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM полностью загружен и разобран");
   require("./modules/main-nav");
 
+  const searchFormToggle = document.querySelector(".header__search-btn");
+  const searcForm = document.querySelector(".header__search");
+
+  searchFormToggle.addEventListener("click", () => {
+    let expanded = searchFormToggle.getAttribute("aria-expanded") === "true";
+    searchFormToggle.setAttribute("aria-expanded", !expanded);
+    searcForm.classList.add("header__search--open");
+  });
+
+  document.addEventListener("click", function (e) {
+    const target = e.target;
+    const itsMenu = target == searcForm || searcForm.contains(target);
+    // const its_btnMenu = target == btnMenu;
+    const menu_is_active = searcForm.classList.contains("header__search--open");
+
+    if (!itsMenu && menu_is_active) {
+      searcForm.classList.remove("header__search--open");
+    }
+  });
+
   const eventsSwiper = new Swiper(".events__slider", {
     // Optional parameters
     slidesPerView: 1,
@@ -54,16 +74,104 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  const textpageGallerySwiper = new Swiper(".textpage__gallery-slider", {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // If we need pagination
+    pagination: {
+      el: ".textpage__gallery-slider-pagination",
+      clickable: true,
+    }, // Navigation arrows
+    navigation: {
+      nextEl: ".textpage__gallery-slider-next",
+      prevEl: ".textpage__gallery-slider-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+    },
+  });
+  const textpageLargeSwiper = new Swiper(".textpage__large-slider", {
+    // Optional parameters
+    slidesPerView: 1,
+    spaceBetween: 20,
+    effect: "fade",
+    fadeEffect: {
+      crossFade: true,
+    },
+    // If we need pagination
+    pagination: {
+      el: ".textpage__large-slider-pagination",
+      clickable: true,
+    }, // Navigation arrows
+    navigation: {
+      nextEl: ".textpage__large-slider-next",
+      prevEl: ".textpage__large-slider-prev",
+    },
+  });
+
+  const newsPageSlider = new Swiper(".news-page__slider--main", {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".news-page__slider-pagination",
+      clickable: true,
+    }, // Navigation arrows
+    navigation: {
+      nextEl: ".news-page__slider-next",
+      prevEl: ".news-page__slider-prev",
+    },
+  });
+
+  const newsGallerySlider = new Swiper(".news-page__slider--gallery", {
+    speed: 400,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: ".news-page__slider-pagination",
+      clickable: true,
+    }, // Navigation arrows
+    navigation: {
+      nextEl: ".news-page__slider-next",
+      prevEl: ".news-page__slider-prev",
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+    },
+  });
+
   var image = document.getElementsByClassName("thumbnail");
   new simpleParallax(image, {
     overflow: true,
-    delay: 0.5,
+    // delay: 0.5,
     transition: "cubic-bezier(0,0,0,1)",
     // maxTransition: 40,
   });
   var imageNoOverflow = document.getElementsByClassName(
     "thumbnail-no-overflow"
   );
+
+  new simpleParallax(imageNoOverflow, {
+    // overflow: true,
+    // delay: 0.5,
+    // transition: "cubic-bezier(0,0,0,1)",
+    // maxTransition: 40,
+  });
 
   // // Parallax effect
   // // Adapted from @ilonacodes article ->  https://link.medium.com/7fFiON6Q1X
