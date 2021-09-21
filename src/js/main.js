37462eb0cfe639896c036756ba2e02d9c89b9a76
alesukhin <alesukhin@museumplus.ru>
2021-09-21 12:43:02 +0300

    fix some fixes

--- src/js/main.js
+++ src/js/main.js
@@ -173,27 +173,12 @@ document.addEventListener("DOMContentLoaded", () => {
     // maxTransition: 40,
   });
 
-  // // Parallax effect
-  // // Adapted from @ilonacodes article ->  https://link.medium.com/7fFiON6Q1X
+  const filterToggle = document.querySelector(".content__filter-toggle");
+  const contentFilterList = document.querySelector(".content__filter-list");
 
-  // // Update : added throttle to increase performance
-  // window.addEventListener("scroll", throttle(parallax, 14));
-
-  // function throttle(fn, wait) {
-  //   var time = Date.now();
-  //   return function () {
-  //     if (time + wait - Date.now() < 0) {
-  //       fn();
-  //       time = Date.now();
-  //     }
-  //   };
-  // }
-
-  // function parallax() {
-  //   var scrolled = window.pageYOffset;
-  //   var parallax = document.querySelector(".parallax");
-  //   // You can adjust the 0.4 to change the speed
-  //   var coords = scrolled * 0.4 + "px";
-  //   parallax.style.transform = "translateY(" + coords + ")";
-  // }
+  if (filterToggle) {
+    filterToggle.addEventListener("click", () => {
+      contentFilterList.classList.toggle("content__filter-list--open");
+    });
+  }
 });
