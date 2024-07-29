0914ed93fd49cc1949495f306fee416764b9bb7a
Alexander Lesukhin <canek.lis@gmail.com>
2024-07-29 15:18:07 +0300

    fix adaptive and add popup

--- src/js/main.js
+++ src/js/main.js
@@ -32,22 +32,22 @@ document.addEventListener('DOMContentLoaded', () => {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
+      clickable: true
     }, // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-prev'
     },
     breakpoints: {
       768: {
         slidesPerView: 1,
-        spaceBetween: 40,
+        spaceBetween: 40
       },
       1200: {
         slidesPerView: 2,
-        spaceBetween: 40,
-      },
-    },
+        spaceBetween: 40
+      }
+    }
   });
 
   const souvenirsSwiper = new Swiper('.souvenirs__slider', {
@@ -57,21 +57,21 @@ document.addEventListener('DOMContentLoaded', () => {
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,
+      clickable: true
     }, // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-prev'
     },
     breakpoints: {
       768: {
-        slidesPerView: 2,
+        slidesPerView: 2
       },
       1200: {
         slidesPerView: 4,
-        spaceBetween: 40,
-      },
-    },
+        spaceBetween: 40
+      }
+    }
   });
 
   const textpageGallerySwiper = new Swiper('.textpage__gallery-slider', {
@@ -81,22 +81,22 @@ document.addEventListener('DOMContentLoaded', () => {
     // If we need pagination
     pagination: {
       el: '.textpage__gallery-slider-pagination',
-      clickable: true,
+      clickable: true
     }, // Navigation arrows
     navigation: {
       nextEl: '.textpage__gallery-slider-next',
-      prevEl: '.textpage__gallery-slider-prev',
+      prevEl: '.textpage__gallery-slider-prev'
     },
     breakpoints: {
       768: {
         slidesPerView: 3,
-        spaceBetween: 20,
+        spaceBetween: 20
       },
       1200: {
         slidesPerView: 2,
-        spaceBetween: 40,
-      },
-    },
+        spaceBetween: 40
+      }
+    }
   });
   const textpageLargeSwiper = new Swiper('.textpage__large-slider', {
     // Optional parameters
@@ -104,17 +104,17 @@ document.addEventListener('DOMContentLoaded', () => {
     spaceBetween: 20,
     effect: 'fade',
     fadeEffect: {
-      crossFade: true,
+      crossFade: true
     },
     // If we need pagination
     pagination: {
       el: '.textpage__large-slider-pagination',
-      clickable: true,
+      clickable: true
     }, // Navigation arrows
     navigation: {
       nextEl: '.textpage__large-slider-next',
-      prevEl: '.textpage__large-slider-prev',
-    },
+      prevEl: '.textpage__large-slider-prev'
+    }
   });
 
   const newsPageSlider = new Swiper('.news-page__slider--main', {
@@ -123,12 +123,12 @@ document.addEventListener('DOMContentLoaded', () => {
     spaceBetween: 20,
     pagination: {
       el: '.news-page__slider-pagination',
-      clickable: true,
+      clickable: true
     }, // Navigation arrows
     navigation: {
       nextEl: '.news-page__slider-next',
-      prevEl: '.news-page__slider-prev',
-    },
+      prevEl: '.news-page__slider-prev'
+    }
   });
 
   const newsGallerySlider = new Swiper('.news-page__slider--gallery', {
@@ -137,34 +137,32 @@ document.addEventListener('DOMContentLoaded', () => {
     spaceBetween: 20,
     pagination: {
       el: '.news-page__slider-pagination',
-      clickable: true,
+      clickable: true
     }, // Navigation arrows
     navigation: {
       nextEl: '.news-page__slider-next',
-      prevEl: '.news-page__slider-prev',
+      prevEl: '.news-page__slider-prev'
     },
     breakpoints: {
       768: {
         slidesPerView: 3,
-        spaceBetween: 40,
+        spaceBetween: 40
       },
       1200: {
         slidesPerView: 4,
-        spaceBetween: 40,
-      },
-    },
+        spaceBetween: 40
+      }
+    }
   });
 
   var image = document.getElementsByClassName('thumbnail');
   new simpleParallax(image, {
     overflow: true,
     // delay: 0.5,
-    transition: 'cubic-bezier(0,0,0,1)',
+    transition: 'cubic-bezier(0,0,0,1)'
     // maxTransition: 40,
   });
-  var imageNoOverflow = document.getElementsByClassName(
-    'thumbnail-no-overflow'
-  );
+  var imageNoOverflow = document.getElementsByClassName('thumbnail-no-overflow');
 
   new simpleParallax(imageNoOverflow, {
     // overflow: true,
@@ -192,7 +190,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //   },
     // },
     Thumbs: {
-      autoStart: false,
+      autoStart: false
     },
 
     animated: false,
@@ -201,12 +199,12 @@ document.addEventListener('DOMContentLoaded', () => {
     dragToClose: false,
     Carousel: {
       on: {
-        change: (that) => {
+        change: that => {
           console.log('blaaa');
-        },
+        }
       },
       Panzoom: {
-        touch: false,
+        touch: false
       },
 
       Navigation: {
@@ -216,8 +214,8 @@ document.addEventListener('DOMContentLoaded', () => {
         `,
         nextTpl: `<svg width="30" height="20" viewBox="0 0 30 20" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd" d="M-1.90735e-06 9.64359C-1.90735e-06 9.35945 0.112873 9.08695 0.313791 8.88603C0.514711 8.68511 0.787216 8.57223 1.07136 8.57223H26.3405L19.5973 1.83124C19.4977 1.73163 19.4187 1.61337 19.3648 1.48322C19.3109 1.35308 19.2831 1.21358 19.2831 1.07271C19.2831 0.931843 19.3109 0.792351 19.3648 0.662204C19.4187 0.532056 19.4977 0.413801 19.5973 0.31419C19.6969 0.21458 19.8152 0.135564 19.9453 0.0816555C20.0755 0.0277466 20.215 0 20.3558 0C20.4967 0 20.6362 0.0277466 20.7664 0.0816555C20.8965 0.135564 21.0148 0.21458 21.1144 0.31419L29.6852 8.88507C29.785 8.98459 29.8642 9.10282 29.9182 9.23298C29.9722 9.36314 30 9.50267 30 9.64359C30 9.78452 29.9722 9.92405 29.9182 10.0542C29.8642 10.1844 29.785 10.3026 29.6852 10.4021L21.1144 18.973C21.0148 19.0726 20.8965 19.1516 20.7664 19.2055C20.6362 19.2594 20.4967 19.2872 20.3558 19.2872C20.215 19.2872 20.0755 19.2594 19.9453 19.2055C19.8152 19.1516 19.6969 19.0726 19.5973 18.973C19.4977 18.8734 19.4187 18.7551 19.3648 18.625C19.3109 18.4948 19.2831 18.3553 19.2831 18.2145C19.2831 18.0736 19.3109 17.9341 19.3648 17.804C19.4187 17.6738 19.4977 17.5556 19.5973 17.456L26.3405 10.715H1.07136C0.787216 10.715 0.514711 10.6021 0.313791 10.4012C0.112873 10.2002 -1.90735e-06 9.92774 -1.90735e-06 9.64359Z" fill="#2E2E2E"/>
-        </svg>`,
-      },
+        </svg>`
+      }
     },
 
     Toolbar: false,
@@ -226,18 +224,17 @@ document.addEventListener('DOMContentLoaded', () => {
     Image: {
       fit: 'contain',
       zoom: false,
-      wheel: null,
+      wheel: null
     },
 
     template: {
-      closeButton:
-        '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.3756 10L0 18.3756L1.6244 20L10 11.6244L18.3756 20L20 18.3756L11.6244 10L20 1.6244L18.3756 0L10 8.3756L1.6244 0L0 1.6244L8.3756 10Z" fill="#414141"/></svg>',
+      closeButton: '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.3756 10L0 18.3756L1.6244 20L10 11.6244L18.3756 20L20 18.3756L11.6244 10L20 1.6244L18.3756 0L10 8.3756L1.6244 0L0 1.6244L8.3756 10Z" fill="#414141"/></svg>'
     },
 
     hideScrollbar: true,
 
     on: {
-      initLayout: (fancybox) => {
+      initLayout: fancybox => {
         const $mainPanel = document.createElement('div');
         $mainPanel.classList.add('fancybox__main-panel');
 
@@ -252,8 +249,8 @@ document.addEventListener('DOMContentLoaded', () => {
         $mainPanel.appendChild(fancybox.$carousel);
 
         fancybox.$backdrop.after($mainPanel);
-      },
-    },
+      }
+    }
   });
   Fancybox.defaults.Image = { zoom: false };
   Fancybox.defaults.ScrollLock = false;
@@ -296,8 +293,28 @@ document.addEventListener('DOMContentLoaded', () => {
   //   '</div>',
   //   });
 
+  const popup = document.querySelector('.popup');
+
+  if (popup) {
+    const timeout = popup.getAttribute('data-timeout');
+    setTimeout(() => {
+      popup.classList.add('is-open');
+      setTimeout(() => {
+        popup.style.opacity = '1';
+      }, 1000);
+
+      popup.querySelector('.popup__close-btn').addEventListener('click', () => {
+        popup.classList.remove('is-open');
+      });
+
+      popup.querySelector('.popup__overlay').addEventListener('click', () => {
+        popup.classList.remove('is-open');
+      });
+    }, timeout);
+  }
+
   const blindVersion = new isvek.Bvi({
     target: '.blind-version',
-    fontSize: 24,
+    fontSize: 24
   });
 });
